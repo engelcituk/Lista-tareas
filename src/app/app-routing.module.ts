01599f910a036013ec1a661f2d2b494d8a32be9e
Encarnacion Cituk Caamal <programaciondyn3.mx@sandos.com>
2019-09-11 09:52:37 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    path: '', loadChildren: () => import('./pages/tabs/tabs.module').then(m => m.TabsPageModule)
   }
 ];
 @NgModule({
--- src/app/pages/agregar/agregar.module.ts
+++ src/app/pages/agregar/agregar.module.ts
@@ -3,15 +3,15 @@ import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { Tab2Page } from './tab2.page';
+import { AgregarPage } from './Agregar.page';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    RouterModule.forChild([{ path: '', component: Tab2Page }])
+    RouterModule.forChild([{ path: '', component: AgregarPage }])
   ],
-  declarations: [Tab2Page]
+  declarations: [AgregarPage]
 })
-export class Tab2PageModule {}
+export class AgregarPageModule {}
